4675f996fcbe7a7297c813b3eeda8dca632940a9
sahinyasasi <sahinya.slc@gmail.com>
2021-02-02 12:12:56 +0530

    setting up API requests

--- src/Pokedex.js
+++ src/Pokedex.js
@@ -1,5 +1,6 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { toFirstCharUpperCase } from "./constants";
+import axios from "axios";
 
 import {
   AppBar,
@@ -12,7 +13,7 @@ import {
   Typography,
 } from "@material-ui/core";
 import { makeStyles } from "@material-ui/core/styles";
-import mockData from "./mockData";
+
 const useStyles = makeStyles({
   pokedexContainer: {
     paddingTop: "20px",
@@ -30,11 +31,29 @@ const useStyles = makeStyles({
 const Pokedex = (props) => {
   const { history } = props;
   const classes = useStyles();
-  const [pokemonData, setPokemonData] = useState(mockData);
+  const [pokemonData, setPokemonData] = useState({});
+  useEffect(() => {
+    axios
+      .get(`https://pokeapi.co/api/v2/pokemon?limit=807`)
+      .then(function (response) {
+        const { data } = response;
+        const { results } = data;
+        const newPokemonData = {};
+        results.forEach((pokemon, index) => {
+          newPokemonData[index + 1] = {
+            id: index + 1,
+            name: pokemon.name,
+            sprite: `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${
+              index + 1
+            }.png`,
+          };
+        });
+        setPokemonData(newPokemonData);
+      });
+  }, []);
 
   const getPokemonCard = (pokemonId) => {
-    const { id, name } = pokemonData[`${pokemonId}`];
-    const sprite = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${id}.png`;
+    const { id, name, sprite } = pokemonData[pokemonId];
     console.log(pokemonData[`${pokemonId}`]);
     return (
       <Grid item xs={12} sm={4} key={pokemonId}>
